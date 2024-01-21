ff2dfb640487a97bf1ddc8b7e5ec2d834f7f4540
nykzaR <nykzar@outlook.com>
2024-01-22 00:25:48 +0530

    added changes

--- jenkins-pipeline-deploy-to-eks-main/terraform/eks-cluster.tf
+++ jenkins-pipeline-deploy-to-eks-main/terraform/eks-cluster.tf
@@ -1,10 +1,10 @@
 module "eks" {
-    source  = "terraform-aws-modules/eks/aws"
-    version = "~> 19.0"
-    cluster_name = "myapp-eks-cluster"
+    source          = "terraform-aws-modules/eks/aws"
+    version         = "~> 19.0"
+    cluster_name    = "myapp-eks-cluster"
     cluster_version = "1.24"
 
-    cluster_endpoint_public_access  = true
+    cluster_endpoint_public_access = true
 
     vpc_id     = module.myapp-vpc.vpc_id
     subnet_ids = module.myapp-vpc.private_subnets
