434fc6be1a34624e6a42ab9356d7fbc047892a44
nykzaR <nykzar@outlook.com>
2024-01-21 15:33:56 +0530

    added changes

---
+++ jenkins-pipeline-deploy-to-eks-main/terraform/inputs.tf
@@ -0,0 +1,12 @@
+variable "vpc_cidr_block" {
+    type    = string
+    default = "10.10.0.0/16"
+}
+
+variable "private_subnet_cidr_blocks" {
+    type    = list(string)
+}
+
+variable "public_subnet_cidr_blocks" {
+    type    = list(string)
+}
--- terraform-code-to-create-jenkins-server-main/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "hyphamentor-app"
-    region = "us-east-1"
-    key = "jenkins-server/terraform.tfstate"
-  }
-}
